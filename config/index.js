366b7f7f196e59098286e812c2b7bf71c3864bf3
sagor ahamed <sagorahamed017997@gmail.com>
2022-11-19 22:14:55 +0600

    updated

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'https://jsonplaceholder.typicode.com/posts?_limit=6' : 'https://localhost:3000'
+export const server = dev ? 'https://jsonplaceholder.typicode.com/posts?_limit=6' : 'https://jsonplaceholder.typicode.com/posts?_limit=6'
--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -18,31 +18,6 @@ const article = ({ article }) => {
   )
 }
 
-// export const getStaticProps = async (context) => {
-//   const res = await fetch(`${server}/api/articles/${context.params.id}`)
-
-//   const article = await res.json()
-
-//   return {
-//     props: {
-//       article,
-//     },
-//   }
-// }
-
-// export const getStaticPaths = async () => {
-//   const res = await fetch(`${server}/api/articles`)
-
-//   const articles = await res.json()
-
-//   const ids = articles.map((article) => article.id)
-//   const paths = ids.map((id) => ({ params: { id: id.toString() } }))
-
-//   return {
-//     paths,
-//     fallback: false,
-//   }
-// }
 
 export const getStaticProps = async (context) => {
   const res = await fetch(
